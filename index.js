7e087366abb7c63ddefc8d154e384d3e60d8bfda
Kirahhellrose <clr@DESKTOP-MVRDDCF.localdomain>
2021-06-09 09:22:53 -0400

    commit tree 6fdfeb720effcb87b36f6b0ba21710ca480a3e38

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+//Multiplication
+let multiply;
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+multiply = num1 * num2 
+
+//Random Integer
+
+let random 
+random = Math.floor(Math.random()+1);
+
+//Mod 
+let mod
+let num3 = (6*6);
+let num4 = 32;
+mod = num3 - num4
+
+//Maximum
+let max 
+max = Math.max(5,6,7,8,9,10,20);
